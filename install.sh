6d8f525778f9f0ce1f50b5f72ecbe979eea69979
Al Zee <z@alz.ee>
2025-09-24 02:25:21 +0800

    nth

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# vim:ft=sh
+
+cp topvpn.conf ~/.topvpn.conf # need edit
+touch ~/.topvpn_pass # need edit
+
+mkdir -p ~/.config/systemd/user/
+
+cp topvpn.service ~/.config/systemd/user/
+systemctl --user daemon-reload
+systemctl --user enable --now topvpn.service
+
+sudo cp topvpnhelper.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable --now topvpnhelper.service
