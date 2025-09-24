690a55f7175e8ec81b7beb4673078a2831a2a3aa
Al Zee <z@alz.ee>
2025-09-24 08:58:21 +0800

    nth

--- install.sh
+++ install.sh
@@ -2,10 +2,17 @@
 #
 # vim:ft=sh
 
+if ! which expect 2> /dev/null; then
+    echo "Error: 'expect' is required for non-interactive login. Install the 'expect' package." >&2
+    exit 1
+fi
+
 cp topvpn.conf ~/.topvpn.conf # need edit
 touch ~/.topvpn_pass # need edit
 
 mkdir -p ~/.config/systemd/user/
+
+cp topvpn ~/.local/bin/
 
 cp topvpn.service ~/.config/systemd/user/
 systemctl --user daemon-reload
